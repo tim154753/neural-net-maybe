495e5756f88f8bb887a3cf6ba9b9fb89b8c886ac
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-06-13 15:48:43 -0700

    Update objectdefs.py

--- objectdefs.py
+++ objectdefs.py
@@ -1,8 +1,9 @@
 import random
 class Neuron:
     neuron_count = 0
-    def __init__(self, value=0, weights=None):
+    def __init__(self, value=0, weights=None, number = 0):
         self.value = value
+        self.number = number
         if weights is None:
             self.weights = []
         else:
@@ -20,23 +21,28 @@ class Neuron:
     def randomize_weights(self):
         for i in range(len(self.weights)):
             self.weights[i] = random.random()
+
     def initialize_value(self, value):
         self.value = value
 
 class Layer:
     layer_count = 0
-    def __init__(self, size):
-        self.neurons = [Neuron() for i in range(size)]
+    def __init__(self, size, layer_connection = None):
+        self.neurons = [Neuron(number=i) for i in range(size)]
+        self.layer_connection = layer_connection
         Layer.layer_count += 1
         self.layer_number = Layer.layer_count
+        self.bias = Neuron(0.0001)
+        self.number_of_connections = 0
 
     def __str__(self):
-        str = f"Layer {self.layer_number} consists of the following neurons:\n"
+        x = f"originates from layer {self.layer_connection.layer_number} " if self.layer_connection is not None else 'is the first layer '
+        str = f"Layer {self.layer_number} {x}and consists of the following neurons:\n"
         i=1
         for neuron in self.neurons:
-            str += f"   Neuron {i} has a value of {neuron.value} and the following list of weights:\n   {neuron.weights}\n"
+            str += f"   Neuron {neuron.number} has a value of {neuron.value} and the following list of weights:\n   {neuron.weights}\n"
             i += 1
-        return str
+        return str + f"      Bias weights: {self.bias.weights}\n"
 
     def randomize_layer_weights(self):
         for neuron in self.neurons:
@@ -45,19 +51,24 @@ class Layer:
     def initialize_neuron_connections(self, number_of_connections):
         for neuron in self.neurons:
             neuron.initialize_weights(number_of_connections)
-        print(f"The neurons in layer {self.layer_number} have now been initialized with weights of {[0 for i in range(number_of_connections)]}")
+        print(f"The neurons (and bias) in layer {self.layer_number} have now been initialized with weights of {[0 for i in range(number_of_connections)]}")
+        self.number_of_connections = number_of_connections
+        self.bias.initialize_weights(number_of_connections)
 
     def initialize_neuron_values(self, list_of_values):
-        for neuron, value in zip(self.neurons, list_of_values[0]):
+        for neuron, value in zip(self.neurons, list_of_values):
             neuron.value = value
 
+    def randomize_bias_weights(self):
+        self.bias.randomize_weights()
+
     def get_neuron_values(self):
-        result  = []
-        for neuron in self.neurons:
-            result.append(neuron.value)
-        return result
+        return [neuron.value for neuron in self.neurons]
+
     def get_neuron_weights(self):
-        result = []
-        for neuron in self.neurons:
-            result.append(neuron.weights)
-        return result
+        return [neuron.weights for neuron in self.neurons]
+
+    def get_bias_weights(self):
+        return self.bias.weights
+
+
