675f9760e07275a356fb600f7298ab487e170dca
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-06-13 15:47:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import random
 
 print("New execution\n")
 
-layer1 = od.Layer(5)
-layer1.initialize_neuron_connections(4)
+layer1 = od.Layer(10)
+layer1.initialize_neuron_connections(5)
 layer1.randomize_layer_weights()
+layer1.randomize_bias_weights()
 for i in range(len(layer1.neurons)):
     layer1.neurons[i].initialize_value(random.random())
 print(layer1)
@@ -14,12 +15,16 @@ print(layer1)
 layer2 = mf.generate_next_layer(layer1)
 layer2.initialize_neuron_connections(10)
 layer2.randomize_layer_weights()
+layer2.randomize_bias_weights()
 print(layer2)
 
 layer3 = mf.generate_next_layer(layer2)
 print(layer3)
 
-error = mf.proto_cost_function(9, layer3)
+error = mf.proto_cost_function(1, layer3, "avg")
 print(error)
+
+
+
 
 
