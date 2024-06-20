84d34a3562b86fc3071c0fb89eef839be45eb866
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-06-19 19:03:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,21 @@ import random
 
 print("New execution\n")
 
+network = od.Network(size = 3)
+
 layer1 = od.Layer(10)
 layer1.initialize_neuron_connections(5)
 layer1.randomize_layer_weights()
 layer1.randomize_bias_weights()
 for i in range(len(layer1.neurons)):
     layer1.neurons[i].initialize_value(random.random())
-print(layer1)
+network.add_layer(layer1)
+network.generate_next_layer(layer1)
+network.layer_list[1].initialize_neuron_connections(10)
+network.layer_list[1].randomize_layer_weights()
+network.layer_list[1].randomize_bias_weights()
+print(network.layer_list[1].get_weighted_inputs())
+network.generate_next_layer(network.layer_list[1])
+network.backprop([0,1,0,0,0,0,0,0,0,0])
+print(network)
 
-layer2 = mf.generate_next_layer(layer1)
-layer2.initialize_neuron_connections(10)
-layer2.randomize_layer_weights()
-layer2.randomize_bias_weights()
-print(layer2)
-
-layer3 = mf.generate_next_layer(layer2)
-print(layer3)
-
-error = mf.proto_cost_function(1, layer3, "avg")
-print(error)
-
-x = mf.find_one_weight_gradient(layer2.neurons[0], layer3.neurons[0],1)
-print(f"\n {x}")
