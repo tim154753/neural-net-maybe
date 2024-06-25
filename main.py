5a6366aeb95f933de3368da2f3c4a22f3f3d65d0
tim154753 <94345691+tim154753@users.noreply.github.com>
2024-06-25 16:08:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ import random
 
 print("New execution\n")
 
-network = od.Network(size = 3)
+'''network = od.Network(size = 3)
 
-layer1 = od.Layer(10)
-layer1.initialize_neuron_connections(5)
-layer1.randomize_layer_weights()
-layer1.randomize_bias_weights()
-for i in range(len(layer1.neurons)):
-    layer1.neurons[i].initialize_value(random.random())
-network.add_layer(layer1)
-network.generate_next_layer(layer1)
-network.layer_list[1].initialize_neuron_connections(10)
+network.generate_first_layer(10)
+network.layer_list[0].initialize_neuron_connections(5)
+network.layer_list[0].randomize_layer_weights()
+network.layer_list[0].randomize_bias_weights()
+for i in range(len(network.layer_list[0].neurons)):
+    network.layer_list[0].neurons[i].initialize_value(random.random())
+network.generate_next_layer(network.layer_list[0])
+network.layer_list[1].initialize_neuron_connections(3)
 network.layer_list[1].randomize_layer_weights()
 network.layer_list[1].randomize_bias_weights()
-print(network.layer_list[1].get_weighted_inputs())
+#print(network.layer_list[1].get_weighted_inputs())
 network.generate_next_layer(network.layer_list[1])
-network.backprop([0,1,0,0,0,0,0,0,0,0])
+print("\nBefore backprop\n")
 print(network)
+network.recursive_backprop([0,1,0,0,0,0,0,0,0,0])
+print("After backprop\n")
+print(network)'''
 
+mf.read_image(0)
